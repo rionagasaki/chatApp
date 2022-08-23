b0432a57cf44218bffdbfb48fdfe7d3aef9da7b2
nagasakirio <naga_ri@icloud.com>
2022-08-23 13:19:37 +0900

    'add-message'

--- stories/BottomInputBar.vue
+++ stories/BottomInputBar.vue
@@ -13,6 +13,7 @@
     <v-btn class="info" small @click="sendMessage" style="sendButtonColor">
       <v-icon>mdi-play</v-icon>{{ sendButtonLabel }}
     </v-btn>
+    <v-btn>aaaa</v-btn>
   </v-card-text>
 </template>
 
